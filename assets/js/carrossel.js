0cc9990dee70bb9e8d22684a61574825e22252ce
Cleber de Souza Abreu <cleber.souza.abreu@gmail.com>
2022-07-31 20:01:30 -0300

    Finalização

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,10 +1,20 @@
-let slideIndex = 0;
+var slideIndex = 0;
 carrossel();
+var slides, pontos, anterior, proximo;
+
+anterior = document.getElementById("botao-anterior-carrossel");
+proximo = document.getElementById("botao-proximo-carrossel");
+anterior.addEventListener("click", function () {
+  mudarSlides(-1);
+});
+proximo.addEventListener("click", function () {
+  mudarSlides(1);
+});
 
 function carrossel() {
-  let i;
-  let slides = document.getElementsByClassName("carrossel");
-  let pontos = document.getElementsByClassName("ponto");
+  var i;
+  slides = document.getElementsByClassName("carrossel");
+  pontos = document.getElementsByClassName("ponto");
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
@@ -17,5 +27,22 @@ function carrossel() {
   }
   slides[slideIndex - 1].style.display = "block";
   pontos[slideIndex - 1].className += " ponto-ativo";
-  setTimeout(carrossel, 4000); // Change image every 2 seconds
+  setTimeout(carrossel, 4000); // Change image every 8 seconds
+}
+
+function mudarSlides(position) {
+  slideIndex += position;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  } else if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < pontos.length; i++) {
+    pontos[i].className = pontos[i].className.replace(" ponto-ativo", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  pontos[slideIndex - 1].className += " ponto-ativo";
 }
